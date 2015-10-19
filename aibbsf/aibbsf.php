89c7cdfa446f443c36ed6341b996fbaa88294640
root <root@hackstubServer.(none)>
2015-10-19 03:01:02 +0100

    aibbsf page first load

---
+++ aibbsf/aibbsf.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * Template Name: AIBBSF
+ * */
+?>
+<?php
+/**
+ *  * @package WordPress
+ *   * @subpackage Default_Theme
+ *    */
+
+
+?>
+
+<!doctype html>
+<html class="no-js" lang="fr">
+<head>
+		<meta charset="UTF-8">
+		<title>Avenir(s) d'internet: Big Brother entre réalité et science-fiction</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="<?php echo( get_stylesheet_directory_uri() . '/aibbsf/aibbsf.css') ?>" media="all">
+		<link rel="stylesheet" href="<?php echo( get_stylesheet_directory_uri() . '/aibbsf/knacss.css') ?>" media="all">
+</head>
+<body>
+	
+	<header id="header" role="banner" class="line pal w800p-ce">
+		<h3 style="color: grey">*** Page en cours d'écriture/design ***</h3>
+		<h1>Avenir(s) d’internet</h1>
+		<h2>Big Brother entre réalité et science-fiction</h2>
+		<p class="">Une série de conférences et d’ateliers à Strasbourg...<br/>
+		...pour comprendre et regarder internet autrement.</p>
+	</header>
+
+	<div id="main" role="main" class="line">
+
+			<div id="sf-intro"><div class="w800p-ce pal">
+				<p>2035, le consortium Google Networks (GN) règne sans partage sur tous les échanges d’informations à l’échelle mondiale. Face à l’effondrement de la souveraineté des états, le groupe est devenu le prestataire de sécurité de tout l’occident, garant de la stabilité en temps de crise : grâce aux implants cérébraux ‘Google imagine’ et aux technologies de surveillance globale de l’agence de sécurité américaine, GN sait tout ce que vous faites, se rappelle ce que vous avez fait, anticipe ce que vous ferez.</p>
+				<p>Toute contestation politique est étouffée avant même d’advenir, grâce aux programmes personnalisés d’anticipation de la déviance et au contrôle par GN des programmes d’éducation. Seul quelques groupes d’activistes vivent coupés du reste de l’humanité en refusant la connexion au réseau Google universel : on les surnomme les Rebelles de l’Intimité...</p>
+				<p class="arial"><br/>Triste récit ? Futurologie manichéenne grotesque ? Ou parabole de notre présent (déjà très) inquiètant ?</p>
+			</div></div>
+
+			<div id="utopie-dystopie"><div class="w800p-ce pal">
+				<img src="<?php echo( get_stylesheet_directory_uri() . '/aibbsf/utopie-dystopie.png') ?>"/>
+			</div></div>
+
+			<div id="retorquer"><div class="w800p-ce pal">
+				<p>On pourrait alors rétorquer : « Ces récits sont certes bien alarmants, mais que puis-je donc faire contre ces dérives ? »</p>
+				<p>C’est simple : n’ayez plus peur de l’eau(*). Pour comprendre les enjeux et voir les choses différement, prenez le temps d’une petite plongée sous la surface…</p>
+			</div></div>
+
+			<div id="nocloud"><div class="w800p-ce pal">
+				<p>Quelles données faites vous circuler sur le réseau ? Comment circulent-elles ?
+Chez qui résident-elles ?</p>
+				<div class="grid-2 ptm">
+					<img class="" src="<?php echo( get_stylesheet_directory_uri() . '/aibbsf/nocloud.png') ?>"/>
+					<div>
+						<p class="">« There is no cloud, just other people’s computer. »</p>
+						<p class="">Le « cloud » n’existe pas, il y a seulement les ordinateurs des autres</p>
+					</div>
+				</div>
+				<p>Avez-vous réellement confiance dans les personnes (Google, Apple, Free, ... autre ) qui hébergent vos données ? Si non, pourquoi ne pas les héberger vous même… ou chez vos amis ?</p>
+			</div></div>
+
+			<div id="pres-projet"><div class="w800p-ce pal">
+				<p>Quelles données faites vous circuler sur le réseau ? Comment circulent-elles ?
+Chez qui résident-elles ?</p>
+				<p>Avez-vous réellement confiance dans les personnes (Google, Apple, Free, ... autre ) qui hébergent vos données ? Si non, pourquoi ne pas les héberger vous même… ou chez vos amis ?</p>
+
+	</div>
+	<footer id="footer" role="contentinfo" class="line pam txtcenter">
+	</footer>
+
+</body>
+</html>
